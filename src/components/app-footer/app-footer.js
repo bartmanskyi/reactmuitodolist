2d81cf0b87a1e809a67f73427e68d2723c690243
Volodymyr Bartmanskyi <bartmanskyi@gmail.com>
2019-07-10 14:53:26 +0300

    fix footer

--- src/components/app-footer/app-footer.js
+++ src/components/app-footer/app-footer.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
 	root: {
 		display: 'flex',
 		flexDirection: 'column',
-		minHeight: '10vh'
+		minHeight: '20vh'
 	},
 	main: {
 		marginTop: theme.spacing(8),
